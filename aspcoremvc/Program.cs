ed34d29c97019343b47c7e35965cfd76b90cf3d7
陈亮文 <1526579511@qq.com>
2018-11-13 14:20:44 +0800

    添加项目文件。

---
+++ aspcoremvc/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aspcoremvc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CreateWebHostBuilder(args).Build().Run();
+            //var config = new ConfigurationBuilder()
+            //  .SetBasePath(Directory.GetCurrentDirectory())
+            //  .AddJsonFile("appsettings.json", optional: true)
+            //  .Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                //.UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                //.UseIISIntegration()//证书，用于https，一般注释掉
+                .UseStartup<Startup>()
+                //.UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
