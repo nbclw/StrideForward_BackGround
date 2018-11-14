2b45955c075fc7a77a3191e23e9af6a40d21416c
陈亮文 <1526579511@qq.com>
2018-11-14 14:18:05 +0800

    111

--- aspcoremvc/Controllers/HomeController.cs
+++ aspcoremvc/Controllers/HomeController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Data;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -25,13 +24,6 @@ namespace aspcoremvc.Controllers
         public string HelloBoy(string name)
         {
             return "hello boy " + name;
-        }
-
-        public string TestDatabase() {
-            string sql = "select count(*) from user";
-            var s = MySqlHelper.ExecuteScalar(sql);
-
-            return s.ToString();
         }
     }
 }
